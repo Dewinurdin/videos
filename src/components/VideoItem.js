51372ec0406fe9411c899041a001e7159f34eef4
Dewinurdin <dewinurdin@gmail.com>
2019-10-12 14:44:30 -0500

    gave img alt tag and video item a key tag

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,11 @@ import './VideoItem.css';
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item"> 
-      <img src={video.snippet.thumbnails.medium.url} className="ui image" />
+      <img 
+        src={video.snippet.thumbnails.medium.url} 
+        className="ui image" 
+        alt={video.snippet.title}
+      />
       <div className="content">
         <div className="header">
           {video.snippet.title} 
