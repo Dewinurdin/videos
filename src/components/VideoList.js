14dace2cfc4ad17238c996a3e21e3ecd24481bf9
Dewinurdin <dewinurdin@gmail.com>
2019-10-12 14:30:47 -0500

    created video detail to show video title and description

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 // passing videos as props 
-// passing onVideoSelect props
+// passing onVideoSelect as props
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
     return <VideoItem onVideoSelect={onVideoSelect} video={video} />
